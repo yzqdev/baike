0169809b7dc172a0e02d4ada63e07eece75dd70f
yzqdev <yzqdev@outlook.com>
2022-06-03 12:42:57 +0000

    Deploying to gh-pages from @ yzqdev/baike@7371aae5ef5274b39c969c95ac27ceac9fe9a44a 🚀

---
+++ assets/index.html.30825257.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./plugin-vue_export-helper.21dcd24c.js";import{o as t,c as r,b as e,d as o}from"./app.1e2f5b70.js";const c={},n=e("h1",{id:"law",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#law","aria-hidden":"true"},"#"),o(" law")],-1),s=[n];function d(_,i){return t(),r("div",null,s)}var f=a(c,[["render",d],["__file","index.html.vue"]]);export{f as default};
